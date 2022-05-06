6c74fbc00118de999a3318f2953fe310d3523c95
Fernando Junior <fernando.junior85@outlook.com>
2022-05-06 15:32:06 -0300

    Fix variable PORT in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-const PORT = 3333
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(process.env.PORT || 3333, () => console.log(`HTTP server running!`))
